f4389cfd8b5c0217635f7504a641da98d5f5e2bc
Riadh Habbachi <riadh.habbachi@gmail.com>
2018-01-07 21:30:57 +0100

    Add support for migrating index-patterns from the main kibana index

--- server/proxy/create_kibana_index.js
+++ server/proxy/create_kibana_index.js
@@ -1,6 +1,8 @@
 import migrateConfig from './migrate_config';
+import migrateIndexPatterns from './migrate_indexpatterns';
 
 module.exports = function (server, index) {
+  server.log(['plugin:own-home', 'debug'], 'create_kibana_index');
   const { callWithInternalUser } = server.plugins.elasticsearch.getCluster('admin');
   return callWithInternalUser('indices.create', {
     index: index,
@@ -12,16 +14,21 @@ module.exports = function (server, index) {
       mappings: server.getKibanaIndexMappingsDsl()
     }
   })
-  .catch(() => {
-    throw new Error(`Unable to create Kibana index "${index}"`);
-  })
-  .then(function () {
-    return callWithInternalUser('cluster.health', {
-      waitForStatus: 'yellow',
-      index: index
-    })
     .catch(() => {
-      throw new Error(`Waiting for Kibana index "${index}" to come online failed.`);
-    }).then(migrateConfig(server, index, [409]));
-  });
+      throw new Error(`Unable to create Kibana index "${index}"`);
+    })
+    .then(function () {
+      return callWithInternalUser('cluster.health', {
+        waitForStatus: 'yellow',
+        index: index
+      })
+        .catch(() => {
+          throw new Error(`Waiting for Kibana index "${index}" to come online failed.`);
+        })
+        .then(migrateConfig(server, index, [409]))
+        .then(function () {
+          migrateIndexPatterns(server, index, [409])
+            .catch(() => { throw new Error(`Migrating Index Patterns to "${index}" failed.`);});
+        });
+    });
 };
